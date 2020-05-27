3e1ff711e43a325bfb751a74a759af023dae0c49
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-05-27 18:12:14 -0500

    added moon and copyright

--- fullmoonalpha/src/components/Home/Home.js
+++ fullmoonalpha/src/components/Home/Home.js
@@ -1,32 +1,33 @@
 import React from "react";
+import moon from "../../assets/images/full_moon.png";
 import { Container, Row, Col } from "reactstrap";
-import './Home.css'
+import "./Home.css";
 
 function Home() {
   return (
-  <Container fluid={true}>
-    <Row id="home_header">
-      <Col>
-      <h1>
-      full moon <a href="index.html">alpha</a>
-     </h1>
-      </Col>
-   
-    </Row>
+    <Container fluid={true}>
+      <Row id="home_header">
+        <Col>
+          <h1>
+            full moon <a href="index.html">alpha</a>
+          </h1>
+        </Col>
+      </Row>
 
-  </Container>
-    // <header class="row" id="home_header">
-    //   <h1 class="col-lg-8">
-    //     full moon <a href="index.html">alpha</a>
-    //   </h1>
-    //   <div class="col-lg-3 offest-lg-1" id="moon_container">
-    //     <img
-    //       src="./assets/images/full_moon.png"
-    //       id="moon_image"
-    //       alt="full moon"
-    //     />
-    //   </div>
-    // </header>
+      <Row id="moon_header">
+        <Col lg={{ size: 6, offset: 6 }} md="12" sm="12">
+          <div id="moon_container">
+            <img src={moon} id="moon_image" alt="full moon" />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <footer className="col-lg-3">
+        {/* <footer className="col-lg-3 col-sm-12 col-xs-12 text-center"> */}
+          <p id="copyright">&copy;gHgraveDesigns 2020</p>
+        </footer>
+      </Row>
+    </Container>
   );
 }
 
